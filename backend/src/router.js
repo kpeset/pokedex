49e92ce9bde2bcab522bfee5aaf2a805f194bf11
Kevin Peset <kevin.peset@gmail.com>
2023-11-12 16:26:17 +0100

    Modifications step 14 : routes et ajout mw

--- backend/src/router.js
+++ backend/src/router.js
@@ -12,34 +12,55 @@ const uploadMiddleware = require("./middlewares/upload");
 const auth = require("./middlewares/auth");
 
 router.get(
-  "/pokemon",
+  "/admin_check",
   auth.checkIfIsAllowed,
   auth.checkIfAdmin,
-  pokemonControllers.browse
+  authServices.allowAccess
 );
+
+router.get("/user_check", auth.checkIfIsAllowed, authServices.allowAccess);
+
+router.get("/pokemon", pokemonControllers.browse);
 router.get("/pokemon/:id", pokemonControllers.read);
 router.get("/pokemon/type/:type", pokemonControllers.searchByType);
 router.get("/pokemon/name/:name", pokemonControllers.searchByName);
 
-router.post("/pokemon", uploadMiddleware.uploadFile, pokemonControllers.add);
-router.put("/pokemon/:id", pokemonControllers.edit);
-router.delete("/pokemon/:id", pokemonControllers.destroy);
+router.post(
+  "/pokemon",
+  uploadMiddleware.uploadFile,
+  auth.checkIfIsAllowed,
+  auth.checkIfAdmin,
+  pokemonControllers.add
+);
+router.put(
+  "/pokemon/:id",
+  auth.checkIfIsAllowed,
+  auth.checkIfAdmin,
+  pokemonControllers.edit
+);
+router.delete(
+  "/pokemon/:id",
+  auth.checkIfIsAllowed,
+  auth.checkIfAdmin,
+  pokemonControllers.destroy
+);
 
-router.get("/users", userControllers.getAllUsers);
+router.get(
+  "/users",
+  auth.checkIfIsAllowed,
+  auth.checkIfAdmin,
+  userControllers.getAllUsers
+);
+
+router.put("/users/:id", auth.hashPassword, userControllers.updateUser);
+
 router.post(
   "/users",
   auth.validateUser,
   auth.hashPassword,
   userControllers.postUser
 );
-router.put("/users/:id", auth.hashPassword, userControllers.updateUser);
 
 router.post("/login", auth.checkEmailIfExist, authServices.verifyPassword);
-
-router.post(
-  "/test",
-  uploadMiddleware.uploadFile,
-  pokemonControllers.checkUpload
-);
 
 module.exports = router;
